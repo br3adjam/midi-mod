d61bbf398ae35107c52b585dddac0d58d3e70711
moth <timothyjyang01@gmail.com>
2025-12-23 19:11:55 -0800

    init

---
+++ src/client/java/com/br3adjam/midi2noteblocks/Midi2noteblocksClient.java
@@ -0,0 +1,10 @@
+package com.br3adjam.midi2noteblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Midi2noteblocksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
