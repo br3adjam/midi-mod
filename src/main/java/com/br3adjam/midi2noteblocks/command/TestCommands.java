4931006157fb3e18f3320cc25566f55fd41b7ce2
br3adjam <timothyjyang01@gmail.com>
2026-01-18 15:21:40 -0800

    essentially remake setblock command and add command to set a noteblock with a certain pitch and immediatley play sound

--- src/main/java/com/br3adjam/midi2noteblocks/command/TestCommands.java
+++ src/main/java/com/br3adjam/midi2noteblocks/command/TestCommands.java
@@ -4,19 +4,26 @@ import com.br3adjam.midi2noteblocks.world.NoteblockControl;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.mixin.command.CommandSourceMixin;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.blocks.BlockInput;
+import net.minecraft.commands.arguments.blocks.BlockPredicateArgument;
+import net.minecraft.commands.arguments.blocks.BlockStateArgument;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.network.chat.SignableCommand;
 import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.NoteBlock;
+import net.minecraft.world.level.block.state.BlockState;
 
 
 public class TestCommands {
@@ -33,14 +40,39 @@ public class TestCommands {
     static public void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(Commands.literal("place")
-                    .then(Commands.argument("pos", BlockPosArgument.blockPos())
-                            .executes(TestCommands::place)));
-        });    }
+                    .then(Commands.argument("block", BlockStateArgument.block(registryAccess))
+                        .then(Commands.argument("pos", BlockPosArgument.blockPos())
+                                .executes(TestCommands::place)
+                        )
+                    )
+            );
+        });
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(Commands.literal("place_noteblock")
+                    .then(Commands.argument("pitch", IntegerArgumentType.integer(0, 24))
+                        .then(Commands.argument("pos", BlockPosArgument.blockPos())
+                                .executes(TestCommands::placeNoteblock)
+                        )
+                    )
+            );
+        });
+    }
+
+    private static int placeNoteblock(CommandContext<CommandSourceStack> context) {
+        NoteblockControl noteblockControl = new NoteblockControl(context.getSource().getLevel());
+        int pitch = IntegerArgumentType.getInteger(context, "pitch");
+        BlockPos pos = BlockPosArgument.getBlockPos(context, "pos");
+        noteblockControl.placeNoteblock(pos, pitch);  // FIXME)) wtf is this vro
+        context.getSource().sendSuccess(() -> Component.literal("Placed Noteblock at %s".formatted(pos)), false);
+        return 1;
+    }
 
     private static int place(CommandContext<CommandSourceStack> context) {
+        NoteblockControl noteblockControl = new NoteblockControl(context.getSource().getLevel());
         BlockPos pos = BlockPosArgument.getBlockPos(context, "pos");
-//        NoteblockControl::placeNoteblock(pos, context.getSource().getLevel());  // FIXME)) wtf is this vro
-        context.getSource().sendSuccess(() -> Component.literal("Placed Noteblock at %s".formatted(pos)), false);
+        BlockInput block = BlockStateArgument.getBlock(context, "block");
+        context.getSource().getLevel().setBlock(pos, block.getState(), 3);
+        context.getSource().sendSuccess(() -> Component.literal("Placed %s at %s".formatted(block,pos)), false);
         return 1;
     }
 
--- src/main/java/com/br3adjam/midi2noteblocks/world/NoteblockControl.java
+++ src/main/java/com/br3adjam/midi2noteblocks/world/NoteblockControl.java
@@ -6,7 +6,17 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.NoteBlock;
 
 public class NoteblockControl {
-    public void placeNoteblock(BlockPos pos, Level level) {
-        level.setBlock(pos, Blocks.NOTE_BLOCK.defaultBlockState(), 3); // TODO)) make sure this works + make pitch changeable
+    Level level;
+    public NoteblockControl(Level level) {
+        this.level = level;
+    }
+
+    public void placeNoteblock(BlockPos pos, int pitch) {
+        level.setBlock(pos, Blocks.NOTE_BLOCK.defaultBlockState()
+                .setValue(NoteBlock.NOTE, Math.max(Math.min(pitch, 24), 0) ),
+            3); // TODO)) make sure this works + make pitch changeable
+//        level.getBlockState(pos).getBlock().triggerEvent(level.getBlockState(pos), level, pos, 0, 0);
+        level.blockEvent(pos, Blocks.NOTE_BLOCK, 0, 0);
+
     }
 }
